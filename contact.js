5fd3369eefd33f49ac75c45104332c13d7535238
Ancy08 <ancymaria6023@gmail.com>
2025-08-23 23:26:57 +0530

    updated contact form

--- contact.js
+++ contact.js
@@ -1,18 +1,10 @@
-// section 1 close nav
-var section1 = document.getElementById("section1")
-var close = document.getElementById("close")
-
-close.addEventListener("click", function () {
-    section1.style.display = "none"
-})
-// menubarjs for contact form
+// sidenav
 var menubar = document.getElementById("menubar")
-var sidenav = document.getElementById("sidenav")
 var closesidenav = document.getElementById("closesidenav")
-
-menubar.addEventListener("click", function () {
+var closenav = document.getElementById("closenav")
+ menubar.addEventListener("click",function(){
     sidenav.style.right = 0
-})
-closesidenav.addEventListener("click", function () {
+ })
+ closesidenav.addEventListener("click",function(){
     sidenav.style.right = "-50%"
-})
+ })
